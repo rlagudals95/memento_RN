e1990a01c3a68e1630d19a646dd3794f108cac6b
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2022-01-19 22:04:04 +0900

    getTimeGap

--- config/customAxios.js
+++ config/customAxios.js
@@ -1,9 +1,9 @@
-import axios, { AxiosInstance } from 'axios';
-import {config} from './confing'
+import axios, { AxiosInstance } from "axios";
+import { config } from "./confing";
 
 export const customAxios = axios.create({
-    baseURL: `${config.localUrl}`, // 기본 서버 주소 입력
-    // headers: {
-    //   access_token: cookies.get('access_token'),
-    // },
-  });
+  baseURL: `${config.bankUrl}`, // 기본 서버 주소 입력
+  // headers: {
+  //   access_token: cookies.get('access_token'),
+  // },
+});
--- src/component/Timezone.js
+++ src/component/Timezone.js
@@ -1,56 +1,66 @@
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
-import { useSelector}  from "react-redux"
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 function Timezone() {
-    const [time, setTime] = useState('loading...');
-    const birthday = useSelector((state) => state.user.birthday ) 
+  const [time, setTime] = useState("loading...");
+  const birthday = useSelector((state) => state.user.birthday);
 
-    // const timeDifference = (_date1, _date2) => {
-    //     const diffDate_1 = _date1 instanceof Date ? _date1 : new Date(_date1);
-    //     const diffDate_2 = _date2 instanceof Date ? _date2 : new Date(_date2);
-      
-    //     const difference = diffDate_1.getTime() - diffDate_2.getTime();
-      
-    //     return difference; // ms
-    // };
+  // const timeDifference = (_date1, _date2) => {
+  //     const diffDate_1 = _date1 instanceof Date ? _date1 : new Date(_date1);
+  //     const diffDate_2 = _date2 instanceof Date ? _date2 : new Date(_date2);
 
-    useEffect(() => {
-        
-        let _birthday = new Date(birthday).getTime();
+  //     const difference = diffDate_1.getTime() - diffDate_2.getTime();
 
-        let dday = new Date(birthday);
-        dday.setFullYear(dday.getFullYear() + 80)
-        
-        setInterval(function() {
-            let _dday = dday.getTime(); // 
-            _birthday++
-            //let today = new Date().getTime();
-            let gap = _dday - _birthday;
-            console.log('이걸로 차이를 : ',gap);
-            
-            let day = Math.ceil(gap / (1000 * 60 * 60 * 24));
-            let hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
-            let sec = Math.ceil((gap % (1000 * 60)) / 1000);
-            
-            let count = "죽음까지 " + day + "일" + hour + "시간 " + min + "분 " + sec + "초 남았습니다.";
-            console.log('날짜',count);
-            setTime(gap);
-        }, 1000);
+  //     return difference; // ms
+  // };
 
-    }, [])
+  useEffect(() => {
+    let dday = new Date(birthday);
+    //디데이 - 벌쓰데이  // - 현재 날짜++
 
-    return (
-        <TimeBox>
-            <TimeCnt>
-                {time}
-            </TimeCnt>
-            <div>
-                초 남았습니다.
-            </div>
-        </TimeBox>
-        )
+    let now = new Date().getTime();
+    dday.setFullYear(dday.getFullYear() + 80);
+
+    setInterval(function () {
+      let timeGap = dday - now; // 태어어난 날로부터 80살 - 현재날짜
+      //console.log("이걸로 차이를 : ", gap);
+      let year = Math.ceil(timeGap / (1000 * 60 * 60 * 24 * 365));
+      let day = Math.ceil(timeGap / (1000 * 60 * 60 * 24));
+      let hour = Math.ceil(
+        (timeGap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let min = Math.ceil((timeGap % (1000 * 60 * 60)) / (1000 * 60));
+      let sec = Math.ceil((timeGap % (1000 * 60)) / 1000);
+
+      // 테스트
+      let _day = parseInt(timeGap / 3600);
+      let _hour = parseInt(timeGap / 3600);
+      let _min = parseInt((timeGap % 3600) / 60);
+      let _sec = timeGap % 60;
+      //console.log("확인 : ", _day, "//", _hour, "//", _min, "//", _sec, "//");
+      let count =
+        "죽음까지 " +
+        day +
+        "일" +
+        hour +
+        "시간 " +
+        min +
+        "분 " +
+        sec +
+        "초 남았습니다.";
+
+      setTime(timeGap);
+      now++;
+    }, 1000);
+  }, []);
+
+  return (
+    <TimeBox>
+      <TimeCnt>{time}</TimeCnt>
+      <div>초 남았습니다.</div>
+    </TimeBox>
+  );
 }
 
 export default Timezone;

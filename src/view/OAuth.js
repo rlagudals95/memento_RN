e8c9a6d2749339a34acd5b2a511ec1d9b5ce29aa
hmk1022 <hm.kim@iworkerman.com>
2022-01-20 17:17:09 +0900

    add-oauth

--- src/view/OAuth.js
+++ src/view/OAuth.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components'
 import { customAxios } from "../../config/customAxios";
+import kakaoLogin from "../../assets/kakao_login.png"
 
 function OAuth() {
-  async function kakaoAuth (){
-    
+  async function kakaoAuth (){ 
     const res = await customAxios.get("/login/getKakaoAuthUrl");
     console.log('소셜로그인 res : ', res.data)
     window.location.href = res.data;
@@ -12,8 +12,13 @@ function OAuth() {
 
   return (
   <div onClick={kakaoAuth}>
-     소셜로그인 
+     <OauthBtn onClick={kakaoAuth} src={kakaoLogin}></OauthBtn> 
   </div>);
 }
 
 export default OAuth;
+
+const OauthBtn = styled.img`
+    cursor: pointer;
+`
+
